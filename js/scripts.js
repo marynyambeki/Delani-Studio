cea046fdc3762866181834dd3b8f767eb43fe7ce
mary <marynyambeki1@gmail.com>
2019-05-03 11:24:59 +0300

    Add initial files

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("p").click(function() {
+      $("img").show();
+    });
+  });
+$(document).ready(function() {
+    $("p").click(function() {
+      $(".walrus-showing").toggle();
+      $(".walrus-hidden").toggle();
+    });
+  });  
