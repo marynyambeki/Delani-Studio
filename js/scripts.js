823f99d9e1aeaa7d74a9302b2ffde1fd6c20cf57
mary <marynyambeki1@gmail.com>
2019-05-05 21:39:52 +0300

    Add js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-    $("p").click(function() {
-      $("img").show();
-    });
+  $(".clickable").click(function() {
+    $("#design-showing").slideToggle();
+    $("#design-hidden").slideToggle();
   });
-$(document).ready(function() {
-    $("p").click(function() {
-      $(".walrus-showing").toggle();
-      $(".walrus-hidden").toggle();
-    });
-  });  
+ });
+ $(document).ready(function() {
+  $(".click").click(function() {
+    $("#dev-showing").slideToggle();
+    $("#dev-hidden").slideToggle();
+  });
+ });
+ $(document).ready(function() {
+  $(".clik").click(function() {
+    $("#pro-showing").slideToggle();
+    $("#pro-hidden").slideToggle();
+  });
+ });
