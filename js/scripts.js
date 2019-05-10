b994889090ba3e19d40348c5eb0c3a713dc4b4bd
mary <marynyambeki1@gmail.com>
2019-05-10 11:33:49 +0300

    Add toogle,hover effect,navigation section,clicks,js,css changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,26 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#design-showing").slideToggle();
-    $("#design-hidden").slideToggle();
+    $(".clickable").click(function() {
+      $("#design-showing").slideToggle();
+      $("#design-hidden").slideToggle()
+    });
   });
- });
- $(document).ready(function() {
-  $(".click").click(function() {
-    $("#dev-showing").slideToggle();
-    $("#dev-hidden").slideToggle();
+  $(document).ready(function() {
+    $(".click").click(function() {
+      $("#dev-showing").slideToggle();
+      $("#dev-hidden").slideToggle()
+    });
+  }); 
+  $(document).ready(function() {
+    $(".able").click(function() {
+      $("#pro-showing").slideToggle();
+      $("#pro-hidden").slideToggle()
+    });
+  
   });
- });
- $(document).ready(function() {
-  $(".clik").click(function() {
-    $("#pro-showing").slideToggle();
-    $("#pro-hidden").slideToggle();
-  });
- });
 
- $(document).ready(function () {
-  return $('#contact-form').submit(function (e) {
-    var email, message, name;
-    name = document.getElementById('inputName');
-    email = document.getElementById('inputEmail');
-    message = document.getElementById('inputMessage');
-    if (!name.value || !email.value || !message.value) {
-      alert('Please check your entries');
-      return false;
-    } else {
-      $.ajax({
-        method: 'POST',
-        url: "http://formspree.io/marynyambeki1@gmail.com",
-        data: $('#contact-form').serialize(),
-        datatype: 'json' });
-
-      e.preventDefault();
-      $(this).get(0).reset();
-      return alert('Message sent');
-    }
+  $(document).ready(function(){
+    $(".col").hover(function(){
+      $(this).children(".hide").toggle();
+      });
   });
-});
+   
